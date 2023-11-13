07e5094e653cacf9c50e880f0a6eeec164edadc3
SeveratheDuck <antipov.aa@phystech.edu>
2023-11-13 17:41:11 +0300

    Better example

--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,7 @@
 
 int main (int /*argc*/, char** /*argv*/)
 {
-    ANA_BinTree tree;
+    ANA_BinTree tree = {};
     ANA_BinTree_Ctor (&tree, "Is it an animal?");
 
     tree.root->right = ANA_BinTree_CtorNode ("Lukashov", nullptr, nullptr, tree.root);
